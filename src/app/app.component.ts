afa333e806684a0ec1857ea10f2b1ba6226b77c8
Zachary Graham <zakgraham@gmail.com>
2018-11-25 17:57:40 -0700

    add tip method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tip-calculator';
+  subtotalWithTax: number;
+  subtotal: number;
+  showSubInput = true;
+  showTaxAdded = false;
+  tipPercentage: number;
+  tipAmount: number;
+  showTipAmount: boolean;
+
+  showWithTax(){
+    this.subtotalWithTax = this.subtotal*1.07;
+    this.showSubInput = !this.showSubInput;
+    this.showTaxAdded = !this.showTaxAdded;
+  }
+
+  calculateTip(){
+    this.tipAmount = (this.tipPercentage/100*this.subtotalWithTax);
+    this.showTipAmount = !this.showTipAmount;
+  }
+
 }
