994187dc19bd8842e657b25add17f2e2f6d46aff
Zachary Graham <zakgraham@gmail.com>
2018-12-01 23:53:16 -0700

    create calculator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
